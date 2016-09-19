acaadbcbd0fa8f38c93ddb8115efb83d0a1d7ae9
fleg <flegmatik.bel@gmail.com>
2016-09-20 00:03:01 +0300

    add format tests

--- test/format.js
+++ test/format.js
@@ -9,4 +9,18 @@ describe('format', function() {
 			jefferson.addFormat('hostname', /.*/);
 		}).to.throwError(/format `hostname` already exists/);
 	});
+
+	it('should add format', function() {
+		jefferson.addFormat('md5', /^[0-9a-f]{32}$/);
+	});
+
+	it('should be ok with added format', function() {
+		jefferson.validate({format: 'md5'}, 'deadbeefdeadbeefdeadbeefdeadbeef');
+	});
+
+	it('should throw error on error', function() {
+		expect(function() {
+			jefferson.validate({format: 'md5'}, 'haha');
+		}).to.throwError(/should match format "md5"/);
+	});
 });
